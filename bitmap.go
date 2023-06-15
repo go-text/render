7cca5c1b31073595c737c2d38548a1f23c2edf1a
Andy Williams <andy@andy.xyz>
2023-06-15 15:25:59 +0100

    Fix coordinates to be offset correctly

--- bitmap.go
+++ bitmap.go
@@ -53,7 +53,7 @@ func (r *Renderer) drawBitmap(g shaping.Glyph, bitmap api.GlyphBitmap, img draw.
 			}
 		}
 
-		rect := image.Rect(int(x), int(y), int(fixed266ToFloat(g.Width)*r.PixScale), int(fixed266ToFloat(g.Height)*r.PixScale))
+		rect := image.Rect(int(x), int(y), int(x+fixed266ToFloat(g.Width)*r.PixScale), int(y+fixed266ToFloat(g.Height)*r.PixScale))
 		scale.NearestNeighbor.Scale(img, rect, sub, sub.Bounds(), draw.Over, nil)
 	case api.JPG, api.PNG, api.TIFF:
 		pix, _, err := image.Decode(bytes.NewReader(bitmap.Data))
@@ -61,8 +61,7 @@ func (r *Renderer) drawBitmap(g shaping.Glyph, bitmap api.GlyphBitmap, img draw.
 			return err
 		}
 
-		h := r.FontSize * r.PixScale
-		rect := image.Rect(int(x), int(y), int(h), int(h))
+		rect := image.Rect(int(x), int(y), int(x+fixed266ToFloat(g.Width)*r.PixScale), int(y+fixed266ToFloat(g.Height)*r.PixScale))
 		scale.BiLinear.Scale(img, rect, pix, pix.Bounds(), draw.Over, nil)
 	}
 
